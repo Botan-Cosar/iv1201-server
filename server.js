09a05d1afd430e01766a1c15727c6f86d00c0602
BotanCosar <55230904+BotanCosar@users.noreply.github.com>
2021-02-03 10:42:42 +0100

    m

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.get("/",(req,res)=>{
     return res.send("Welcome");
 })
 
-const port=5000;
-const server=app.listen(port,"localhost",()=>{
+const port=process.env.PORT||5000;
+const server=app.listen(port,()=>{
     console.log(`Server is up at port: ${port}`);
 });
 
