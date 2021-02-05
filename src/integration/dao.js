7b26d18df3f1180047169ef5b10d5d334129f8ca
Kodeama <Kodeama@users.noreply.github.com>
2021-02-05 11:16:59 +0100

    production and developement database env variables

--- src/integration/dao.js
+++ src/integration/dao.js
@@ -13,12 +13,25 @@ class DAO {
    * Creates a new instance and connects to the database.
    */
   constructor() {
-    this.database = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASS,
-        {host: process.env.DB_HOST, dialect: process.env.DB_DIALECT,port:process.env.DB_PORT}
-    );
+    if(process.env.NODE_ENV === "production"){
+      console.log("production");
+      this.database = new Sequelize(
+        process.env.DATABASE_URL, {
+          dialect: 'postgres',
+          protocol: 'postgres',
+          dialectOptions: {
+              ssl: true
+          }
+      });
+    }
+    else{
+      this.database = new Sequelize(
+          process.env.DB_NAME,
+          process.env.DB_USER,
+          process.env.DB_PASS,
+          {host: process.env.DB_HOST, dialect: process.env.DB_DIALECT,port:process.env.DB_PORT}
+      );
+    }
     Person.createModel(this.database);
   }
 
